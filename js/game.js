936c6a5e594cdd2e6493f0b732d896f837bff96d
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-07-27 15:22:02 -0300

    adicionando timer e nome jogador

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector(".grid");
+const spanPlayer = document.querySelector(".player");
+const timer = document.querySelector(".timer");
 
 const characters = [
     'beth',
@@ -26,7 +28,8 @@ const checkEndGame = () => {
     const disabledCards = document.querySelectorAll(".disabled-card");
 
     if(disabledCards.length === 20){
-        
+        clearInterval(this.loop);
+        alert(`Parabéns ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
     }
 }
 
@@ -100,4 +103,18 @@ const loadGame = () => {
     });
 }
 
-loadGame();
+const startTimer = () => {
+    this.loop = setInterval(() => {
+        const currentTime = Number(timer.innerHTML);
+        timer.innerHTML = currentTime + 1;
+    }, 1000);
+}
+
+window.onload = () => {
+    const playerName = localStorage.getItem('player');
+
+    spanPlayer.innerHTML = playerName;
+
+    startTimer();
+    loadGame();
+}
